da5cc83fe869efee1abe6cba1cd788accf678d10
Athan Clark <athan.clark@gmail.com>
2017-11-16 14:15:37 -0700

    stm chans supported

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,10 @@ import Control.Concurrent.Chan (newChan, writeChan, readChan)
 import Control.Concurrent.Chan.Extra (debounceStatic, intersperseStatic)
 import Control.Concurrent.Async (async)
 import Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
+import Control.Concurrent.STM (atomically)
+import Control.Concurrent.STM.TChan (newTChan, writeTChan, readTChan)
+import qualified Control.Concurrent.STM.TChan.Extra as TChan
+import Control.Concurrent.STM.TMVar (newEmptyTMVar, putTMVar, takeTMVar)
 
 main :: IO ()
 main = do
@@ -49,5 +53,50 @@ main = do
   putStrLn "writing 2..."
   writeChan incoming 2
   _ <- takeMVar lock
+
+  pure ()
+
+
+  outgoing <- atomically newTChan
+  (incoming, _) <- TChan.debounceStatic 1000000 outgoing
+
+  lock <- atomically newEmptyTMVar
+
+  _ <- async $ forever $ do
+    x <- atomically $ readTChan outgoing
+    print x
+    atomically $ putTMVar lock ()
+
+  putStrLn "writing 1..."
+  atomically $ writeTChan incoming 1
+
+  _ <- atomically $ takeTMVar lock
+
+  putStrLn "writing 2..."
+  atomically $ writeTChan incoming 1
+  atomically $ writeTChan incoming 2
+
+  _ <- atomically $ takeTMVar lock
+
+
+
+  outgoing <- atomically newTChan
+  (incoming, _, _) <- TChan.intersperseStatic 1000000 (pure 0) outgoing
+
+  lock <- atomically newEmptyTMVar
+
+  _ <- async $ forever $ do
+    x <- atomically $ readTChan outgoing
+    print x
+    atomically $ putTMVar lock ()
+
+  threadDelay 3000000
+
+  putStrLn "Writing 1..."
+  atomically $ writeTChan incoming 1
+  _ <- atomically $ takeTMVar lock
+  putStrLn "Writing 2..."
+  atomically $ writeTChan incoming 2
+  _ <- atomically $ takeTMVar lock
 
   pure ()
